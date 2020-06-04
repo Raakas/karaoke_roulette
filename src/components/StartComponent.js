02340b80073049ce5c7f5cb8743cef16df71e054
Raakas <arttu.tiiro@gmail.com>
2020-06-04 14:22:02 +0300

    style text input

--- src/components/StartComponent.js
+++ src/components/StartComponent.js
@@ -5,8 +5,9 @@ const StartComponent = (props) => {
     return (
         <div className="start">
             <h1>Karaoke Roulette</h1>
-            <p>Choose genre</p>
-            <input type="text" value={props.value} onChange={props.handleChange}/>
+            <p>The karaoke party machine</p>
+            <br/>
+            <input type="text" value={props.value} onChange={props.handleChange} placeholder="Type in genre or artist"/>
             <br/>
             <Link to="player" onClick={props.fetchTracklist}><button className="button button-green">Sing</button></Link>
         </div>
