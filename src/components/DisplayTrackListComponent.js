68963e0c799e621b1f3178483a9f928dee32f921
Raakas <arttu.tiiro@gmail.com>
2021-04-02 12:13:38 +0300

    display track list component

---
+++ src/components/DisplayTrackListComponent.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const DisplayTrackListComponent = (props) => {
+    return (
+        <div className="tracklist">
+            {props.trackList.length > 0
+                ? <>
+                    <p className="tracklist__track">Woah! Keep in mind Youtube quality might be shit...</p>
+                    <br/>
+                    {props.trackList.map((item, index) => (
+                        <p 
+                            key={index}
+                            className="tracklist__track"
+                        >
+                            {item}
+                        </p>
+                    ))}
+                </>
+                : <p className="tracklist__track">No tracks yet</p>
+            }
+        </div>
+    )
+}
+
+export default DisplayTrackListComponent
