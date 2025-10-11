616e017d860f9630e5b3c9031283b31c45f6859a
Raakas <arttu.tiiro@gmail.com>
2025-10-11 19:49:23 +0300

    remove search result dropdown

--- src/components/searchBarComponent.tsx
+++ src/components/searchBarComponent.tsx
@@ -1,15 +1,10 @@
-import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import {
-  LastFmApiResponse,
   updateSearchParam,
   updateTrackList,
   selectSearchType,
   selectSearchParam,
 } from '../store/appSlice'
-import { ApiFetchService } from '../services/fetchService'
-
-const apiFetchService = new ApiFetchService()
 
 const SearchBar = () => {
   const dispatch = useDispatch()
@@ -17,69 +12,20 @@ const SearchBar = () => {
   const searchType = useSelector(selectSearchType)
   const searchParam = useSelector(selectSearchParam)
 
-  const [searchMatches, setSearchMatches] = useState<Array<LastFmApiResponse>>()
-
   const isEmpty = (param: string): boolean => !param || param.length === 0
 
-  const updateSearchParameter = (event: any) => {
-    if (isEmpty(event)) {
+  const updateSearchParameter = (value: string) => {
+    if (isEmpty(value)) {
       dispatch(updateSearchParam(''))
+      dispatch(updateTrackList([]))
       return
     }
 
-    let searchParam: string = event.target
-      ? event.target.value.toLowerCase().trim()
-      : event
-    dispatch(updateSearchParam(searchParam))
-
-    if (isEmpty(searchParam)) {
-      dispatch(updateTrackList([]))
-    }
-  }
-
-  const fetchSearcResultsFromAPI = async (value: string) => {
-    if (isEmpty(value)) {
-      resetSearch()
-    } else {
-      await apiFetchService
-        .searchBarAPI(value, searchType)
-        .then((result: any): Array<LastFmApiResponse> => {
-          setSearchMatches(result)
-          return result
-        })
-    }
-  }
-
-  const setSearchResult = async (result: string) => {
-    setSearchMatches([])
-    updateSearchParameter(result)
-    const newTrackList = await apiFetchService.lastFmTrackFetcher(
-      searchParam,
-      searchType,
-    )
-    dispatch(updateTrackList(newTrackList))
-  }
-
-  const getQueryParameter = (value: string) => {
-    if (isEmpty(value)) {
-      resetSearch()
-    } else if (value.length > 6) {
-      let results
-      if (searchMatches && searchMatches.length > 0) {
-        results = searchMatches?.filter((a: LastFmApiResponse) =>
-          a.name.toLowerCase().includes(value.toLowerCase()),
-        )
-      }
-      setSearchMatches(results)
-    } else {
-      fetchSearcResultsFromAPI(value)
-    }
-    updateSearchParameter(value)
+    dispatch(updateSearchParam(value))
   }
 
   const resetSearch = () => {
     dispatch(updateSearchParam(''))
-    setSearchMatches([])
   }
 
   return (
@@ -89,7 +35,7 @@ const SearchBar = () => {
           className="song-input"
           type="text"
           value={searchParam}
-          onChange={(e) => getQueryParameter(e.target.value)}
+          onChange={(e) => updateSearchParameter(e.target.value)}
           placeholder={`Type in ${searchType}`}
         />
         {searchParam && (
@@ -98,21 +44,6 @@ const SearchBar = () => {
           </a>
         )}
       </div>
-      {searchMatches && searchMatches.length > 0 && (
-        <div className="results">
-          {searchMatches?.map((item: LastFmApiResponse) => (
-            <p
-              key={item.name}
-              className="text-tiny"
-              onClick={() => setSearchResult(item.name)}>
-              {item.name}
-            </p>
-          ))}
-        </div>
-      )}
-      {searchParam && searchMatches && (
-        <p>Nothing found, but try to get track list !</p>
-      )}
     </div>
   )
 }
